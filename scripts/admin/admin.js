de5a5f0085db7d8c3410b19b669ff6888853b573
Janek Lauer <janek.lauer@andugo.io>
2024-06-13 15:12:37 +0200

    update

---
+++ scripts/admin/admin.js
@@ -0,0 +1,5 @@
+import CMS from "@sveltia/cms";
+// Initialize the CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
+// CMS.registerPreviewTemplate("my-template", MyTemplate);
